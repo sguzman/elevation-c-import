a884c6f2a05bc37f9ac7e5361ed67211f7835fab
Ruediger Ranft <_rdi_@web.de>
2011-10-08 23:14:16 +0200

    Added the used tags.

--- wikiparser.c
+++ wikiparser.c
@@ -1,4 +1,28 @@
 #include "wikiparser.h"
+#include <stdlib.h>
+
+#define TAGDEF(name, _a, _b) name,
+enum CurrentTag{
+#include "tags.inc"
+};
+#undef TAGDEF
+
+struct TagInfo
+{
+    const char* tag;
+    enum CurrentTag root;
+};
+
+#define TAGDEF(_, tag, root) {tag, root},
+static struct TagInfo const tags[] ={
+#include "tags.inc"
+};
+#undef TAGDEF
+
+struct ParserState
+{
+    enum CurrentTag tag;
+};
 
 static void levStartElement(void* context, const xmlChar* name, const xmlChar** attribs)
 {
