b17d4c34306618397e71eefb4cf78880924c7ee3
Ruediger Ranft <_rdi_@web.de>
2011-10-14 19:23:06 +0200

    Added the function to convert the wikipedia date into a git date.

--- gitwriter.c
+++ gitwriter.c
@@ -2,6 +2,8 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
+#include <assert.h>
 
 static int rewrite_filename_chars(char* dest, const char* source)
 {
@@ -52,26 +54,43 @@ static void create_filename(struct DynString* dest, struct DynString const* sour
     rewrite_filename_chars(dest->data, source->data);
 }
 
-static void wikiDateToGitDate(struct DynString* dest, struct DynString const* source)
+static void wikiDateToGitDate(char* dest, const char* source)
 {
+    static const char* month_names[] = {"Jan", "Feb", "Mar", "Apr", "May",
+                                   "Jun", "Jul", "Aug", "Sep", "Oct",
+                                   "Nov", "Dec"};
+    /* Source: <timestamp>2001-05-30T11:37:26Z</timestamp>
+                          01234567890123456789
+       Dest:   05 May 11:37:26 2001
+               01234567890123456789*/
+    strncpy(dest, source + 8, 2);
+    unsigned month = source[6] - '0';
+    if((month < 3) && (source[5] == '1'))
+    {
+        month += 10;
+    }
+    assert(month <= 12);
+    strncpy(dest+3, month_names[month-1], 3);
+    strncpy(dest+7, source+11, 8);
+    strncpy(dest+16, source, 4);
 }
 
-static void print_author(struct RevData const* revision, struct DynString const* date)
+static void print_author(struct RevData const* revision, const char* date)
 {
     if(!stringIsEmpty(&revision->user))
     {
-        printf("author %s <%s> %s\n", revision->user.data, revision->user.data, date->data);
+        printf("author %s <%s> %s\n", revision->user.data, revision->user.data, date);
     }
 }
 
 void commit_rev(struct RevData const* revision, struct DynString const* title, bool start_branch)
 {
     static struct DynString file_name;
-    static struct DynString git_date;
-    wikiDateToGitDate(&git_date, &revision->time);
+    static char git_date[] = "22 Feb 05:42:23 2097";
+    wikiDateToGitDate(git_date, revision->time.data);
     create_filename(&file_name, title);
     printf("commit refs/heads/%s\n", file_name.data);
-    print_author(revision, &git_date);
+    print_author(revision, git_date);
     printf("committer Jon Doe <jon.doe@example.com> now\n");
     if(!stringIsEmpty(&revision->comment))
     {
